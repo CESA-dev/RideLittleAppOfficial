a5f11d12264b0b8c3dd7678970e52080d6aa6f3d
Hongyu Wang <hongyuwang@wirelessprv-10-194-130-17.near.illinois.edu>
2018-04-15 16:07:54 -0500

    rating modified

--- pages/rating/rating.js
+++ pages/rating/rating.js
@@ -1,5 +1,14 @@
 Page({
   data: {
+    rideTime: {
+      hours: 2,
+      minutes: 45
+    },
+    driverInfo: {
+      avatarUrl: 
+      'http://s3.amazonaws.com/nvest/Blank_Club_Website_Avatar_Gray.jpg',
+      nickName: '老司机'
+    },
     star: 0,
     starMap: [
       '非常差',
@@ -16,3 +25,6 @@ Page({
     });
   }
 });
+
+// 接收：顺风车时长 司机头像 id
+// 发送：该次顺风车评分
